c441ad3f161f42742a9bc4e0c0c801eddf625daf
iamstarlee <13253620092@163.com>
2025-01-14 15:45:15 +0800

    assign input of image

--- predict.py
+++ predict.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         while True:
             img = input('Input image filename:')
             try:
-                image = Image.open(img)
+                image = Image.open("img/street.jpg")
             except:
                 print('Open Error! Try again!')
                 continue
